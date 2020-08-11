fd2d0109858b03684aa0bc71a3a3bcff45a0e048
Luiz Poleto <luiz.poleto@gmail.com>
2020-08-11 09:35:23 -0400

    Update inserts.sql

--- data/inserts.sql
+++ data/inserts.sql
@@ -2,9 +2,9 @@ INSERT INTO customer (id, email, name) VALUES (1, 'a@a.com', 'customer A');
 INSERT INTO customer (id, email, name) VALUES (2, 'b@b.com', 'customer B');
 INSERT INTO customer (id, email, name) VALUES (3, 'c@c.com', 'customer C');
 
-INSERT INTO product (id, name, amount) VALUES (1, 'Boné', 25);
-INSERT INTO product (id, name, amount) VALUES (2, 'Bola', 120);
-INSERT INTO product (id, name, amount) VALUES (3, 'Bermuda', 60);
-INSERT INTO product (id, name, amount) VALUES (4, 'Camiseta', 80);
-INSERT INTO product (id, name, amount) VALUES (5, 'Chuteira', 130);
+INSERT INTO product (id, name, amount) VALUES (1, 'Hat', 25);
+INSERT INTO product (id, name, amount) VALUES (2, 'Ball', 120);
+INSERT INTO product (id, name, amount) VALUES (3, 'Shorts', 60);
+INSERT INTO product (id, name, amount) VALUES (4, 'Shirt', 80);
+INSERT INTO product (id, name, amount) VALUES (5, 'Shoes', 130);
 
